4bd14339992e2a8dbee3b128f198ce9a3b58f3b7
pradeepkays <pradeep.k@kaysharbor.com>
2023-07-23 20:02:03 +0530

    New commit

--- index.php
+++ index.php
@@ -46,6 +46,7 @@
     <title>PetTracker - <?=$row['petName']?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="./assets/images/PT.svg">
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
